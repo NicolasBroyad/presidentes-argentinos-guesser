fe0c1f2e4f9eaafd70d3c90574806bba5da926f8
mateodelafuente <mateodelafuente@uca.edu.ar>
2025-09-09 17:28:10 -0300

    Corregido error Milei (mas de un anio)

--- Presidente.js
+++ Presidente.js
@@ -6,9 +6,12 @@ const Presidente = function(nombre, segundoNombre, apellido, periodo, deFacto, i
     this.deFacto = deFacto; //true o false
     this.imagen = imagen;
 
-    this.estuvoMasDeUnAnio = function(){
-        return this.periodo.calcularTiempoEnMandato() >= 365;
-    }
+    this.estuvoMasDeUnAnio = function() {
+        const fechaFin = this.periodo.fin || new Date(); // si fin es null, usa hoy
+        const tiempoEnDias = (fechaFin - this.periodo.inicio) / (1000 * 60 * 60 * 24);
+        return tiempoEnDias >= 365;
+    };
+
 
     this.esDeFacto = () => this.deFacto;
 }
